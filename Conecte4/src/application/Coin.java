24f555d6f6bf0dc859784e07dde3e7af8952e981
diogo6mateus <100148905+diogo6mateus@users.noreply.github.com>
2022-06-30 15:52:40 +0100

    D_ClassCoin_Code

--- Conecte4/src/application/Coin.java
+++ Conecte4/src/application/Coin.java
@@ -3,12 +3,48 @@ package application;
 import javafx.scene.canvas.GraphicsContext;
 import multiplayer.Jogador;
 
-public class Coin {	//classe que representa uma das moedas do jogo.
-	
-	public Coin() {
+public class Coin { 
+    private GraphicsContext gc; 
+    private Jogador jogador;    
+    private double posX, posY;  
+    private double width, height;   
+    private double velY = 0;    
+    private double graV = 10;   
+    private double finY;    
+    
+    public Coin(GraphicsContext gc, double posX, double posY, double finY, double width, double height, Jogador jogador) {
+        this.gc = gc;
+        this.posX = posX * width;
+        this.posY = posY * height;
+        this.finY = finY * height;
+        this.width = width;
+        this.height = height;
+        this.jogador = jogador;
+    }   //construtor simples
+    
+    public void update() { 
+        if(velY<2.5) return;    
+        velY+=graV;
+        if(velY<2.5) return;    
+        posY += velY;   
+        if(posY>=finY) { 
+            posY = finY;    
+            velY*=-0.2; 
+        }
+    }
+    
+    public void show() {    
+        gc.drawImage(jogador.getCoin(), posX, posY, width, height);
+    }
+    
+    public Jogador getJogador() {   
+        return this.jogador;
+    }
 
-	}	
-	
-	public void update() { }
-		
+    public String getPos() {    
+        return posX + " " + posY;
+    }
+    
 }
+
+
