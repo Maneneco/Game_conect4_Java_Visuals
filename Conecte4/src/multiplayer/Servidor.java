26548b5e9b2be465bde5d4630cffda67e0bf06ba
diogo6mateus <100148905+diogo6mateus@users.noreply.github.com>
2022-07-01 14:09:20 +0100

    D_ServidorClass_IniciarServidor

--- Conecte4/src/multiplayer/Servidor.java
+++ Conecte4/src/multiplayer/Servidor.java
@@ -1,6 +1,25 @@
 package multiplayer;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class Servidor {	//servidor de conexao
-
+public class Servidor {	
+	private ServerSocket server; 
+	private Socket client; 
+	
+	Scanner entrada; 
+	PrintStream saida; 
+	
+	
+	public Servidor() throws IOException {
+		server = new ServerSocket(4444); 
+		client = server.accept(); 
+		entrada = new Scanner(client.getInputStream()); 
+		saida = new PrintStream(client.getOutputStream());
+	}
+	
+	
 }
