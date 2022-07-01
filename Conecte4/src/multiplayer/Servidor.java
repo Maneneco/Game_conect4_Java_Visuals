f472293b2b5bf2446990df9a4f2c4c75f67cd206
diogo6mateus <100148905+diogo6mateus@users.noreply.github.com>
2022-07-01 14:10:29 +0100

    D_ServidorClass_IniciarServidorComentado

--- Conecte4/src/multiplayer/Servidor.java
+++ Conecte4/src/multiplayer/Servidor.java
@@ -6,18 +6,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Servidor {	
-	private ServerSocket server; 
-	private Socket client; 
+public class Servidor {	//servidor de conexao
+	private ServerSocket server; //socket do tipo servidor
+	private Socket client; //referencia ao cliente conectado
 	
-	Scanner entrada; 
+	Scanner entrada; //buffers de entrada e saida  de dados.
 	PrintStream saida; 
 	
 	
 	public Servidor() throws IOException {
-		server = new ServerSocket(4444); 
-		client = server.accept(); 
-		entrada = new Scanner(client.getInputStream()); 
+		server = new ServerSocket(4444); //o server aguarda conexoes na porta 4444
+		client = server.accept(); //ao receber uma conexao, guarda o cliente
+		entrada = new Scanner(client.getInputStream()); //a entrada e saida do cliente sao estabelecidos como buffers
 		saida = new PrintStream(client.getOutputStream());
 	}
 	
