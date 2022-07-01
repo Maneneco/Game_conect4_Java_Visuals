7a6eac1681cfdb253e219505eb538e8eb5f807a6
marqu <carloscz@ipvc.pt>
2022-07-01 05:20:56 +0100

    Update

--- Conecte4/src/multiplayer/MultiplayerUtil.java
+++ Conecte4/src/multiplayer/MultiplayerUtil.java
@@ -4,25 +4,27 @@ import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
-public class MultiplayerUtil {	//utilitario de conexao
-	private Jogador j1, j2, atual = null; //guarda os jogadores bem como qual é o jogador atual
-	private Servidor servidor=null; //guarda um possivel servidor
-	private Cliente cliente=null; //ou cliente
-	private Boolean exited; // e uma referencia que indica que o jogador local desistiu.
-	private String dadosRecebidos = ""; //string que guarda dados recebidos
-	private String dadosAEnviar = ""; //string que guarda dados a enviar
+public class MultiplayerUtil {	
+	private Jogador j1, j2, atual = null; 
+	private Servidor servidor=null; 
+	private Cliente cliente=null; 
+	private Boolean exited;
+	private String dadosRecebidos = "";
+	private String dadosAEnviar = "";
 	
 	
 	public MultiplayerUtil(String ip, String username, Boolean exited) throws IOException, UnknownHostException {
 		if(ip.isEmpty()) {	
-			
+			servidor = new Servidor(); 
 			j1 = new Jogador(username, 1, 0); 
 			servidor.enviarDados(username); 
+			String dados=null; 
 			while(dados==null) { 
 				dados = servidor.recebeDados();	
+			}
 			j2 = new Jogador(dados, -1, 0); 
 			atual = j1; 
-		} else {	
+		} else {	/
 			cliente = new Cliente(ip);  
 			
 			
@@ -32,13 +34,14 @@ public class MultiplayerUtil {	//utilitario de conexao
 			}
 			
 			j1 = new Jogador(dados, 1, 0);	
+			cliente.enviarDados(username); 
 			
 			j2 = new Jogador(username, -1, 0); 
 			atual = j1; 
 		}
 		this.exited = exited;	
 		dadosRecebidos = "";
-
+		recebeDadosContinuos();	
 		enviaDadosContinuos();
 	}
 	
@@ -51,7 +54,7 @@ public class MultiplayerUtil {	//utilitario de conexao
 	}
 	
 	public String recebeDados() {	
-		if(!dadosRecebidos.isEmpty()) {
+		if(!dadosRecebidos.isEmpty()) { 
 			String cpy = dadosRecebidos; 
 			dadosRecebidos = ""; 
 			return cpy; 
@@ -62,13 +65,51 @@ public class MultiplayerUtil {	//utilitario de conexao
 	public void enviaDadosContinuos() {	
 		new Thread() {
 			@Override
-			public void run() {	
-		
+			public void run() {
+				while(!exited) { 
+					if(servidor!=null) { 
+						if (dadosAEnviar.isEmpty())	servidor.enviarDados("#"); 
+						else {
+							servidor.enviarDados(dadosAEnviar); 
+							dadosAEnviar = ""; 
+						}
+					} else {	
+						if (dadosAEnviar.isEmpty())	cliente.enviarDados("#");	
+						else {
+							cliente.enviarDados(dadosAEnviar); 
+							dadosAEnviar = "";
+						}
+					}
+				}
+				if(servidor!=null) {	
+					servidor.encerra();
+				} else {
+					cliente.encerra();
+				}
 				currentThread().interrupt();	
 			}
 		}.start();
 	}
 	
+	public void recebeDadosContinuos() {	
+		new Thread() {
+			@Override
+			public void run() {
+				while(!exited) { 
+					if(servidor!=null) {	
+						String s = servidor.recebeDados(); 
+						if (s.contains("#")) continue; 
+						dadosRecebidos = s;	
+					} else { 
+						String s = cliente.recebeDados();
+						if (s.contains("#")) continue;
+						dadosRecebidos = s;
+					}
+				}
+				currentThread().interrupt();	
+			}
+		}.start();
+	}
 	
 	
 
@@ -80,8 +121,11 @@ public class MultiplayerUtil {	//utilitario de conexao
 		}
 	}
 	
+	public void addMove() {	
+	
+	}
 
-	public void mudaJogador() {
+	public void mudaJogador() {	
 		this.atual = this.atual==j1?j2:j1;
 	}
 
@@ -93,6 +137,7 @@ public class MultiplayerUtil {	//utilitario de conexao
 		try {
 			return Inet4Address.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
